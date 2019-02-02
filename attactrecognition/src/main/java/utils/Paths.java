15dec4342b298fa7df51953c6e37c2b3a208535d
matimac1994 <matiwsrh1994@gmail.com>
2019-02-02 22:58:37 +0100

    add test and train files

--- attactrecognition/src/main/java/utils/Paths.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public class Paths {
-    public static final String PIMA_DATA_CSV = "./pima_data.csv";
-    public static final String PIMA_TRAIN_DATA_CSV = "./pima_train_data.csv";
-    public static final String PIMA_TEST_DATA_CSV = "./pima_test_data.csv";
-}
